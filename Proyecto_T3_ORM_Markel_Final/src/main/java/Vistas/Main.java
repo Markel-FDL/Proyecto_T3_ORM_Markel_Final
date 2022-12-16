ca6c0acc4a3c0437df6dc56e6f04a61af88facbf
Markel Fernandez de Labastida <markel.fernandezdelabastida@ikasle.egibide.org>
2022-12-16 14:05:25 +0100

    Continuar con la instalacion de Hibernate

---
+++ Proyecto_T3_ORM_Markel_Final/src/main/java/Vistas/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Vistas;
+
+/**
+ *
+ * @author 9fdam05
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       MenuPrincipal vp = new MenuPrincipal();
+        vp.setLocationRelativeTo(null);
+        vp.setVisible(true);
+
+        
+        //GestionProyectos gp = new GestionProyectos();
+        //gp.setVisible(true);
+    }
+}
